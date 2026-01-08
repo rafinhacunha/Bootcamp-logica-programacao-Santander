949930c980bcb6567b364e01a01485c413cfc0e8
Rafael Henrique da Cunha <167113156+rafinhacunha@users.noreply.github.com>
2026-01-07 22:22:43 -0300

    Add files via upload

---
+++ 01-conceitos-basicos/tipos-de-dados-e-variaveis/5-exercicio-variaveis.js
@@ -0,0 +1,9 @@
+//aprendendo tipo de variaveis com pokemon
+let nomePokemon = "Pikachu"
+let nivelPokemon = 20
+let pontosDeVidaPokemon = 45
+let pokemonSexo = "M"
+let selecionavel = true
+
+// String são variveis que armazenam textos, Number são variaveis que armazenam números, Boolean são variaveis lógicas que armazenam true e false (ligado ou desligado)
+
