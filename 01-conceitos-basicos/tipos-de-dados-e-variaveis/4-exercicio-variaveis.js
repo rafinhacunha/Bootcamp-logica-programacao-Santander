4f52d96fd7ea1489429216a1264ba68f5e014042
Rafael Henrique da Cunha <167113156+rafinhacunha@users.noreply.github.com>
2026-01-07 22:12:08 -0300

    Refactor variable declarations and logging output
    Refeito utilizando os conceitos aprendidos

--- 01-conceitos-basicos/tipos-de-dados-e-variaveis/4-exercicio-variaveis.js
+++ 01-conceitos-basicos/tipos-de-dados-e-variaveis/4-exercicio-variaveis.js
@@ -1,5 +1,16 @@
+//declarando variaveis que podem se alterar
 let poteCafe = "Café Pilão"
 let poteAcucar = "Açucar Cristal"
 let poteBiscoito = "Biscoito Polvilho"
 
+//variavel constante
+const mensagemRepetida = "Na cozinha tem: "
+
+//output/saída sem usar a variavel constante
 console.log ("Na cozinha tem: " + poteCafe + ", " + poteAcucar + ", " + poteBiscoito)
+
+//alterando a variavel após a declaração
+poteCafe = "Café Barroco"
+
+//output com a variavel constante e com a alteracao da variavel poteCafe
+console.log(mensagemRepetida + poteCafe + ", " + poteAcucar + ", " + poteBiscoito)
