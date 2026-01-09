c03a617bf5bb26824ec91206cbfa275847bf713e
Rafael Henrique da Cunha <167113156+rafinhacunha@users.noreply.github.com>
2026-01-09 00:18:50 -0300

    Add files via upload

---
+++ 01-conceitos-basicos/vetores-e-matrizes/1-exercicio-vetores-e-matrizes.js
@@ -0,0 +1,14 @@
+//alocando strings em vetor
+let pokemon = ["Pikachu", "Charmander", "Bulbassauro"]
+
+//metodo que decrementa o vetor (diminui uma posição, automaticamente "eliminando" o ultimo elemento do vetor)
+pokemon.pop()
+
+//output do vetor todo
+console.log(pokemon)
+
+//metodo que eliminado a primeira posicao do vetor
+pokemon.shift()
+
+//output = "Charmander"
+console.log(pokemon)
---
+++ 01-conceitos-basicos/vetores-e-matrizes/2-exercicio-vetores-e-matrizes.js
@@ -0,0 +1,13 @@
+//alocando vetores para criar um vetor bidimensional (matriz)
+let timePokemon = [
+["pikachu", "M", "lvel 1"],
+["charmander", "F", "lvel 5"]
+]
+
+//trabalhando com matrizes em saidas
+console.log("O Pokemon " + timePokemon[0][0] + " é do sexo: " + timePokemon[0][1])
+
+
+let pokemon = ["Pikachu", "Charmander", "Bulbassauro"]
+//utilizando propriedades da lingugagem, no caso da propriedade lenght é para coletar o tamanho do vetor
+console.log(pokemon.length)
