72119b8476b8fe490b6d3bcca0a29c188f557717
Rafael Henrique da Cunha <167113156+rafinhacunha@users.noreply.github.com>
2026-01-09 21:53:52 -0300

    Add files via upload

---
+++ 02-trabalhando-com-operadores/operadores-aritmeticos/1-exercicio-operadores-aritmeticos.js
@@ -0,0 +1,26 @@
+let idade = 30 
+console.log("Valor da variavel: " + idade)
+
+//somando a variavel a 6 e atribuindo esse valor a variavel "idade"
+idade += 6
+console.log("Valor da variavel somando ela a 6: " + idade)
+
+//operacao de subtracao
+idade = 30
+idade -= 6
+console.log("Valor da variavel ao subtrair ela a 6: " + idade)
+
+let num = 4
+let num1 = 12
+
+//trabalhando com multiplicacao
+let produto = num * num1
+console.log(produto)
+
+let notaDoMercado = 50 
+//trabalhando com divisao
+console.log(notaDoMercado/2)
+
+//operacao de modulo (operacao que pega o resto da divisao dos reespectivos valores)
+let calculo = 10 % 3
+console.log(calculo)
