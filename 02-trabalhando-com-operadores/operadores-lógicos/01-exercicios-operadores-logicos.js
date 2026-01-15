e71c622d68e9abf452ede1e7bdb5e8c0f2b5a7c7
Rafael Henrique da Cunha <167113156+rafinhacunha@users.noreply.github.com>
2026-01-15 00:10:50 -0300

    Add files via upload

---
+++ 02-trabalhando-com-operadores/operadores-lógicos/01-exercicios-operadores-logicos.js
@@ -0,0 +1,13 @@
+// AND (&&) é um operador lógico que só funciona se duas condições forem verdadeiras
+
+let idade = 18
+let vistoVerificado = false
+let resultado =  (idade >= 18) && (vistoVerificado === true) 
+
+console.log(resultado)
+
+let moedasColetadas = 99
+let item = "estrela"
+let resultado_ = (moedasColetadas >= 100) && (item === "estrela")
+
+console.log(resultado_)
